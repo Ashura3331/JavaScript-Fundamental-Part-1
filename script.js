4ffb94425fe0f4b741b23badf028cf164ca23c14
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-13 21:45:49 +0600

    new variables

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let js = 'amazing';
-if (js === "amazing") alert ("JavaScript is Fun!");
+// if (js === "amazing") alert ("JavaScript is Fun!");
 console.log (40 + 8 + 23 - 10);
+
+console.log("Jonas");
+
+// We should write clean and clear Variable 
+let myFirstJob = 'Programmer'; // Good one
+let myCurrentJob = 'Teacher'; // Good one
+
+let job1 = 'programmer'; // Bad one
+let job2 = 'teacher'; // Bad one
+
+console.log(myFirstJob, myCurrentJob); // Good one
+console.log( job1, job2); // Bad one
