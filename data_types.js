b5163ed57989743f90492f6effabc68b624b2f71
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-14 07:00:17 +0600

    new file

---
+++ data_types.js
@@ -0,0 +1,33 @@
+
+true;
+
+// Boolean Data Type
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+// Booleand Data type
+console.log(typeof true);
+
+// Number Data type
+console.log(typeof 23);
+
+// String Data type
+console.log(typeof 'Jonas');
+
+// String Data type
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+//Undefined Data type
+let year;
+console.log(year);
+console.log(typeof year);
+
+//Number data Type
+year = 1991;
+console.log(typeof year);
+
+
+//Ojbect Data type or Null Data type
+console.log(typeof null);
