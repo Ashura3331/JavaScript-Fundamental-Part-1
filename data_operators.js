bf8654e24c5c6a46ace909679ab84146e0a87a22
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-14 21:23:50 +0600

    new changes of Operators

--- data_operators.js
+++ data_operators.js
@@ -1,6 +1,28 @@
-
+// Math Operator
 const now = 2037;
 const ageJonas = now - 1991;
 
 const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
+
+// 2 ** 3 means 2 to the power of 3 = 2 * 2* 2
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+
+// Assignment Operator
+let x = 10 + 5;
+
+
+x += 10; //x = x + 10
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah);
