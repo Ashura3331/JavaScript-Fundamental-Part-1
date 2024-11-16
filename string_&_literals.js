49cf19ed72152425774e3ec8791271f1dd8e2d0d
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-16 12:16:57 +0600

    string and literature added

---
+++ string_&_literals.js
@@ -0,0 +1,7 @@
+
+const firstName = 'Ash';
+const job = 'Programmer';
+const birthYear = 1999;
+const year = 3000;
+
+const ash = "I'm " + firstName + ', a ' + (year - birthYear) + 'years old ' + job + '!';
