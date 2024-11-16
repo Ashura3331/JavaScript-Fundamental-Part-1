7a67de09ce8533895b086e636c003d9b20775897
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-16 13:04:37 +0600

    bectrice uses in two console

--- string_&_literals.js
+++ string_&_literals.js
@@ -7,3 +7,13 @@ const year = 3000;
 const ash = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
 
 console.log(ash);
+
+const ashNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(ashNew);
+
+console.log("Hello world!\n\ My name is Ash\n\ I'm a new Programmer!");
+
+
+console.log(`Hello world!
+My name is Ash
+I'm a new Programmer!`);
