666b492de4f3f36c4649fb7d3a986f776ca34c75
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-25 20:52:10 +0600

    new data about this file

--- truth_false.js
+++ truth_false.js
@@ -5,3 +5,17 @@ console.log(Boolean(undefined));
 console.log(Boolean('Jonas'));
 console.log(Boolean({}));
 console.log(Boolean());
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all:");
+}else {
+    console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+    console.log("Yay! Height is defined");
+}else {
+    console.log("Height is Undefined");
+}
