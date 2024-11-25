e70a7aac5de516fac0476d84ff7d824660ac235a
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-25 20:46:36 +0600

    new update

--- truth_false.js
+++ truth_false.js
@@ -1,1 +1,7 @@
+// 5 falsy valuses: 0, '', undefined, null, NaN
+
 console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean());
