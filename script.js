ec63ae73019710d480e7b1b1b1353806696781a6
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-25 20:44:21 +0600

    truthy and falsy

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ let job2 = 'teacher'; // Bad one
 console.log(myFirstJob, myCurrentJob); // Good one
 console.log( job1, job2); // Bad one
 
+
+
+
+// Type conversion
+
 const inputYear = '1991';
 console.log(Number(inputYear));
 console.log(Number(inputYear) + 18);
+console.log(Number('Jonas'));
+console.log(String(23), 23);
+
+
+// Type coercion
+
+console.log('I am ' + 23 + 'years old');
+console.log('23' - '10' - 3);
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
