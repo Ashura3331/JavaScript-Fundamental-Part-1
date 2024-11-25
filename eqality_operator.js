9f4391407d3aacbe8a9be878d8ce7138a1f421ec
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-25 21:10:23 +0600

    number

--- eqality_operator.js
+++ eqality_operator.js
@@ -4,3 +4,15 @@ const age = 18;
 if (age === 18) {
     console.log("You are ready to drive a car🚗");
 }
+if (age == '18') {
+    console.log("You are ready to drive a car🚗");
+}
+
+const favourite = Number(prompt("What's your favourite number?"));
+
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 13) {// '23' == 23
+    console.log(`Cool! ${favourite} is an amazing number!`);
+}
