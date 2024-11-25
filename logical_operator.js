929a591bc499925ae0ec907024006a2f98cf76c2
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-25 21:38:46 +0600

    new data

--- logical_operator.js
+++ logical_operator.js
@@ -3,3 +3,14 @@ const hasDriversLicense = true; // A
 const hasGoodVision = false; // B
 
 console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(hasDriversLicense && !hasGoodVision); // I use two logical operator in this operatoin
+
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if(shouldDrive) {
+    console.log("Sarah is able to drive!");
+}else {
+    console.log("Some else should drive...");
+}
