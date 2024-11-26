8a9698453750a28a64611c81f41da3c387764436
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-26 15:22:27 +0600

    structure of switch

---
+++ switch.js
@@ -0,0 +1,25 @@
+
+const day = 'tuesday';
+
+switch(day) {
+    case 'monday':
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case  'tuesday':
+        console.log("Prepare theory vidoes");
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log("Write code example");
+        break
+    case 'friday':
+        console.log("Record videoes");
+        break
+    case 'saturday':
+    case 'sunday':
+        console.log("Enjoy the weekend :)")
+        break;
+    default:
+        console.log("Not a valid day");
+}
