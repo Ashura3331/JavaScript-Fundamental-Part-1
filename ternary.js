932557a95dbf13ec016a2958e974ddd504a59004
Ahsan Habib <freelancerashaa@gmail.com>
2024-11-26 16:07:48 +0600

    new data update

---
+++ ternary.js
@@ -0,0 +1,8 @@
+
+
+const age = 18;
+
+age >= 18 ? console.log("I like to drink pure water") : console.log("I like to drink soft drinks");
+
+const drink = age >= 18 ? 'water' : 'drinks';
+console.log("drink");
